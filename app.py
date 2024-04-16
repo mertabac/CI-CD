f66c3fdb7f1f2a350a19abf474ea4ca0f602e21a
Mert <mert_292@hotmail.com>
2024-04-16 20:07:40 +0300

    upp

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import yfinance as yf
 import mplfinance as mpf
 import numpy as np
 import math
-
+import matplotlib.pyplot as plt
 def plot_candlestick(symbol, period="1mo", interval="1h"):
 
     ticker = yf.Ticker(symbol)
